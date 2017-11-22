2ea66dc7aa02333e2edb0833e592ee4ca3caf5d1
Maxim Petrusevich <maks1464@gmail.com>
2017-11-23 01:59:31 +0300

    Created project structure, Added home page skeleton

---
+++ src/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Home.css'
+
+class Home extends Component {
+
+    render() {
+        return(
+            <div className="home_background">
+            </div>
+        );
+    }
+}
+
+export default Home;
