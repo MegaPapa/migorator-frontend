0106cbc0da6b14e307765416eca6d26a9969e29e
Maxim Petrusevich <maks1464@gmail.com>
2017-11-24 03:35:30 +0300

    Added auth div

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './Home.css'
+import Auth from './auth/Auth.js'
 
 class Home extends Component {
 
     render() {
         return(
             <div className="home_background">
+                <div className="auth_header">
+                    <p>Sign In to Continue</p>
+                </div>
+                <Auth />
             </div>
         );
     }
